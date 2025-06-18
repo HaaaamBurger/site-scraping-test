a92c9dbf51a64cbf7004d8ecaa191f76c8ad15a5
HaaaamBurger <gemasclashes@gmail.com>
2025-06-18 17:47:24 +0300

    project set-up

---
+++ src/main/java/org/main/sitescrapingtest/SiteScrapingTestApplication.java
@@ -0,0 +1,13 @@
+package org.main.sitescrapingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteScrapingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteScrapingTestApplication.class, args);
+	}
+
+}
